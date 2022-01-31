009676c32490ce0a506c1772d93f90486240a10b
Mary Martinez <mary.diana.martinez@gmail.com>
2022-01-31 12:11:52 -0800

    Added form and remove event listeners

--- app.js
+++ app.js
@@ -34,14 +34,26 @@ function resetStats() {
 }
 
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
     // Step 1 - add code to track each submission to the stats
+    const statsData = new FormData(form);
     // Create an object to "model" you statistic like the following:
     // { player: 'Bob', points: 2 }
+    const playerSave = {
+        player: statsData.get('player'),
+        points: Number(statsData.get('points'))
+    };
+
+    stats.push(playerSave);
+    renderStats();
+
     // Hint -- create the object from the form, push it onto the stats array, then call renderStats
 });
 
 remove.addEventListener('click', () => {
     // Step 2 -- add code to allow users to remove the most recent stat
+    stats.pop();
+    renderStats();
     // Hint -- how do you remove an element from an array?
     // Hint -- how can we rerender the stats using a function above?
 });
@@ -49,7 +61,15 @@ remove.addEventListener('click', () => {
 save.addEventListener('click', () => {
     // Step 3 - add code to allow users to save the state
     // Loop through the list of stats and add up the total points scored
+    let sum = 0;
+    for (let stat of stats) {
+        sum += stat.points;
+    }
+    // console.log('This is the sum', sum);
     // Create a new object with the game number and the total points
+    // const gameData = {
+    //     number:
+    // }
     // { number: games.length + 1, totalPoints: totalPoints }
     // Push the new object onto the games array then call renderGames
     // reset the stats with resetStats
